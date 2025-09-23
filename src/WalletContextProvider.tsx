837b040b48ac24347019dbd73dd9e4c475210562
olaniyi <arokoyueb11@gmail.com>
2025-09-23 15:46:32 +0100

    fixing wallet issue

--- src/WalletContextProvider.tsx
+++ src/WalletContextProvider.tsx
@@ -1,21 +1,31 @@
 import { useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl } from "@solana/web3.js";
-// Import the wallet adapter styles
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-const WalletContextProvider = ({ children }:any) => {
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'
+const WalletContextProvider = ({ children }: any) => {
   const network = WalletAdapterNetwork.Devnet;
- 
-  // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  
+
+  // Register the wallets you want to support
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]} autoConnect>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
